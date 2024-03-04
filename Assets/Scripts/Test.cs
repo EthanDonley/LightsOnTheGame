52b726caef416f471c4b567020aa957893651e2e
SagarMathur <s_mathur4@u.pacific.edu>
2024-03-03 19:18:00 -0800

    Added greeting and changed color of square

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("My name is Sagar Mathur");
     }
 
     // Update is called once per frame
