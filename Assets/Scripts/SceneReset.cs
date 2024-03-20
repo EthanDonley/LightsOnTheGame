b8975ccb7d65177b38c84083c3f4263780196425
fruiteyloops <e_donley@u.pacific.edu>
2024-03-20 15:57:49 -0700

    Added Reset system (Press R)

---
+++ Assets/Scripts/SceneReset.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReset : MonoBehaviour
+{
+    void Update()
+    {
+        // Check if the 'R' key is pressed
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // Reload the current scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
