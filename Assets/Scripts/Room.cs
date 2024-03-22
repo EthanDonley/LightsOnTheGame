9d45783438d2bc330e68a4daf78e9d956c3b214c
fruiteyloops <e_donley@u.pacific.edu>
2024-03-22 15:39:18 -0700

    Offset no longer happens upon restart or starting the game

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -6,24 +6,40 @@ public class Room : MonoBehaviour
 {
     public GameObject virtualCam;
     public float offset = 8f;
+    private bool canOffsetPlayer = false;
 
+
+    private void Start()
+    {
+        StartCoroutine(DelayOffsetActivation());
+    }
+
+    IEnumerator DelayOffsetActivation()
+    {
+        yield return new WaitForSeconds(0.1f);
+        canOffsetPlayer = true;
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
-            Vector2 playerPos = other.transform.position;
-            Vector2 roomPos = transform.position;
-
-            if (playerPos.x < roomPos.x)
+            if (canOffsetPlayer)
             {
-                playerPos.x += offset;
-            }
-            else
-            {
-                playerPos.x -= offset;
+                Vector2 playerPos = other.transform.position;
+                Vector2 roomPos = transform.position;
+
+                if (playerPos.x < roomPos.x)
+                {
+                    playerPos.x += offset;
+                }
+                else
+                {
+                    playerPos.x -= offset;
+                }
+
+                other.transform.position = playerPos;
             }
 
-            other.transform.position = playerPos;
             virtualCam.SetActive(true);
         }
     }
