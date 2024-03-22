a3e03f67f62e7cf24ea885f4fe72d4723e104e8f
fruiteyloops <e_donley@u.pacific.edu>
2024-03-22 15:20:04 -0700

    More camera adjustments

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public GameObject virtualCam;
-    public float offset = 4f;
+    public float offset = 8f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
