9f01ef7aefb6ea6869b341e238efb11ae7a7eaef
fruiteyloops <e_donley@u.pacific.edu>
2024-03-22 14:53:44 -0700

    Added player offset for camera switch to make the player closer to the room upon switching levels

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public GameObject virtualCam;
+    public float offset = 4f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
+            Vector2 playerPos = other.transform.position;
+            Vector2 roomPos = transform.position;
+
+            if (playerPos.x < roomPos.x)
+            {
+                playerPos.x += offset;
+            }
+            else
+            {
+                playerPos.x -= offset;
+            }
+
+            other.transform.position = playerPos;
             virtualCam.SetActive(true);
         }
     }
