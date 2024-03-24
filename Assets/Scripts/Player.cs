d935afe979985ff2958dd2989085b30e012a3fcf
fruiteyloops <e_donley@u.pacific.edu>
2024-03-24 16:15:57 -0700

    Slight value adjustments on Raycasting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -45,7 +45,7 @@ public class Player : MonoBehaviour
             Vector2 checkpointPosition = checkpoint.position;
             transform.position = new Vector2(checkpointPosition.x, checkpointPosition.y);
         }
-        RayXPositions = new float[] { -0.1f, 0.0f, 0.1f, };
+        RayXPositions = new float[] { -0.13f, 0.0f, 0.11f, };
     }
 
     void Update()
